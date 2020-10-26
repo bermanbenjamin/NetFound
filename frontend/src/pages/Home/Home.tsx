ce72a057c1e4157433e47dcdf20f63634816ba6a
tiko <carloslopessf@gmail.com>
2020-10-26 09:30:20 -0300

    HomePage

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { FiChevronRight } from 'react-icons/fi';
+
+import './home.css';
+
+import logoHome from '../../assets/logo-home.svg';
+
+function Home() {
+    return (
+
+        <div id="page-background">
+            <div className="content">
+                <img src={logoHome} alt="Home" />
+
+                <main>
+                    <h1>Tá cansado de assistir os mesmos<br />filmes e séries né?</h1>
+                    <span>Ache as melhores programações pro estilo que combina com você!</span>
+
+
+                    <Link to="#" className="enter-app">
+                        <div className="text-button">
+                            <span>VAMOS LÁ</span>
+                        </div>
+                        
+                        <div className="icon">
+                            <FiChevronRight size={40} color="#ffffff" />
+                        </div>
+                    </Link>
+                </main>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
