f1d17c83bdd4beba654196e8534343a2832c4ea7
Berman Benjamin <bermanschultz29@gmail.com>
2020-10-26 11:34:05 -0300

    'frontend-home-responsive'

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { FiChevronRight } from 'react-icons/fi';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
 
-import './home.css';
+import "./home.css";
 
-import logoHome from '../../assets/logo-home.svg';
+import logoHome from "../../assets/logo-home.svg";
 
 function Home() {
-    return (
+  return (
+    <div id="page-background">
+      <main>
+        <img src={logoHome} alt="Home" />
+        <div className="content">
+          <h1>
+            Tá cansado de assistir os mesmos filmes e séries né?
+          </h1>
+          <span>
+            Ache as melhores programações pro estilo que combina com você!
+          </span>
 
-        <div id="page-background">
-            <div className="content">
-                <img src={logoHome} alt="Home" />
+          <Link to="#">
+            <button className="enter-app">
+              <div className="text-button">
+                <span>VAMOS LÁ</span>
+              </div>
 
-                <main>
-                    <h1>Tá cansado de assistir os mesmos<br />filmes e séries né?</h1>
-                    <span>Ache as melhores programações pro estilo que combina com você!</span>
-
-
-                    <Link to="#" className="enter-app">
-                        <div className="text-button">
-                            <span>VAMOS LÁ</span>
-                        </div>
-                        
-                        <div className="icon">
-                            <FiChevronRight size={40} color="#ffffff" />
-                        </div>
-                    </Link>
-                </main>
-            </div>
+              <div className="icon">
+                <FiChevronRight size={40} color="#ffffff" />
+              </div>
+            </button>
+          </Link>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
 
 export default Home;
